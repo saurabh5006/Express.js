7693c5fe330c295910f8ebdec7a3b5a9fc22c684
saurabh5006 <itsupport3@powerhf>
2025-04-29 12:14:06 +0530

    added new file

---
+++ math.js
@@ -0,0 +1,15 @@
+let sum = (a,b)=> a+b;
+let mul = (a,b)=> a*b;
+let g = 9.8;
+let PI = 3.14;
+
+let obj = {
+    sum : sum,
+    mul : mul,
+    g : g,
+    PI : PI
+}
+
+// console.log("Saurabh");
+
+module.exports = obj; 
---
+++ script.js
@@ -0,0 +1,4 @@
+let vale = require("./math"); 
+
+console.log(vale.sum(2,3));
+console.log(vale.mul(3,4));
