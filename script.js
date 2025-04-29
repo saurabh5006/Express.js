d3d63f4f1f3d3e3d928958d228c5af0fbdd104f0
saurabh5006 <itsupport3@powerhf>
2025-04-29 12:36:10 +0530

    added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let vale = require("./math"); 
+let vale = require("./math"); // we are using math method and function
 
 console.log(vale.sum(2,3));
 console.log(vale.mul(3,4));
+
+console.log(valu.PI = 23);
